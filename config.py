83244347aa14a6ce66240c5e3ce595bcfd744c49
mtproo <96996066+mtproo@users.noreply.github.com>
2022-08-20 21:29:12 -0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ PORT = 443
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "20000002000022200000000200002200"
+    "tg":  "32b920dffb51643028e2f6b878d4eac1"
 }
 
 MODES = {
@@ -20,8 +20,8 @@ MODES = {
 
 # The domain for TLS mode, bad clients are proxied there
 # Use random existing domain, proxy checks it on start
-TLS_DOMAIN = "www.Yadesh.ir"
+TLS_DOMAIN = "www.we-have.english"
 
 # Tag for advertising, obtainable from @MTProxybot
-AD_TAG = "8aaddd60c4328bc57a7d0b52c57e4906"
+AD_TAG = "d1cd833e886347155cc292f899891ef7"
 
